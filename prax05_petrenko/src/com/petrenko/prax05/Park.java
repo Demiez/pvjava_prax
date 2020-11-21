2195e520ee63a072dd5649bc6d5b28bb38978ef8
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-21 15:33:47 +0200

    prax05 - initial setup

---
+++ prax05_petrenko/src/com/petrenko/prax05/Park.java
@@ -0,0 +1,5 @@
+package com.petrenko.prax05;
+
+public class Park {
+
+}
---
+++ prax05_petrenko/src/com/petrenko/prax05/ParkApp.java
@@ -0,0 +1,10 @@
+package com.petrenko.prax05;
+
+public class ParkApp {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+
+}
