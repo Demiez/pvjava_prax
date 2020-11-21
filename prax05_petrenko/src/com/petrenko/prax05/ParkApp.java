1b36ca5625e527c16652e0efb4e7567fe3d02aa1
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-11-21 16:23:38 +0200

    prax05 - park class

--- prax05_petrenko/src/com/petrenko/prax05/ParkApp.java
+++ prax05_petrenko/src/com/petrenko/prax05/ParkApp.java
@@ -3,8 +3,11 @@ package com.petrenko.prax05;
 public class ParkApp {
 
     public static void main(String[] args) {
-	// write your code here
+        Park park = new Park("Shevchenko Park");
+        park.addAttraction("Carnival Ride", "8AM-10PM", 7.99f);
+        park.addAttraction("Baloon Race", "8AM-8PM", 4.99f);
+        park.addAttraction("Hyper Booster", "8AM-8PM", 8.99f);
+
+        park.displayParkInfo();
     }
-
-
 }
