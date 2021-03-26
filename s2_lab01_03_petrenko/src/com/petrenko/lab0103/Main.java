8effec62ae836785cc7a2b16e87bef774650babc
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-26 09:58:21 +0200

    s2_lab01_03 - implemented line by line reader

---
+++ s2_lab01_03_petrenko/src/com/petrenko/lab0103/Main.java
@@ -0,0 +1,8 @@
+package com.petrenko.lab0103;
+
+public class Main {
+    public static void main(String[] args) {
+        MyDemoSwing f = new MyDemoSwing("My Title");
+        f.setVisible(true);
+    }
+}
---
+++ s2_lab01_03_petrenko/src/com/petrenko/lab0103/MyDemoSwing.java
@@ -0,0 +1,68 @@
+package com.petrenko.lab0103;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class MyDemoSwing extends JFrame {
+
+    public JButton inputButton;
+    public JLabel label;
+    public JTextField textField;
+
+    public MyDemoSwing(String title) {
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("text.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNextLine()){
+            list.add(s.nextLine());
+        }
+        s.close();
+        
+        JPanel panel = new JPanel();
+        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        panel.setLayout(new BorderLayout());
+
+        label = new JLabel("Проста програма");
+        inputButton = new JButton("Кнопка");
+        textField = new JTextField("");
+
+        panel.add(textField, BorderLayout.NORTH);
+        panel.add(label);
+        panel.add(inputButton, BorderLayout.SOUTH);
+
+        setContentPane(panel);
+        setPreferredSize(new Dimension(300, 150));
+        pack();
+
+
+
+        inputButton.addActionListener(new ActionListener() {
+            int counter = 0;
+            public void actionPerformed(ActionEvent arg0) {
+                textField.setText(list.get(counter++));
+                if (list.size() == counter) {
+                    counter = 0;
+                }
+            }
+        });
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
