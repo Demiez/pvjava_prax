6c1cd43a02d5312be234632a60a691080c0f7c6e
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-27 15:07:26 +0200

    s2_lab02_13 - HashSet implementation for text

--- s2_lab02_13_petrenko/src/com/petrenko/lab0213/HashSetOps.java
+++ s2_lab02_13_petrenko/src/com/petrenko/lab0213/HashSetOps.java
@@ -1,4 +1,33 @@
 package com.petrenko.lab0213;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class HashSetOps {
+    public HashSet<String> set = new HashSet<String>();
+
+    public HashSetOps() {
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("text.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (s.hasNext()){
+            this.set.add(s.next().toLowerCase(Locale.ROOT));
+        }
+        s.close();
+    }
+
+    public void printSet() {
+        System.out.println("Current set for text.txt:");
+
+        for(String item: this.set){
+            System.out.println(item);
+        }
+    }
 }
