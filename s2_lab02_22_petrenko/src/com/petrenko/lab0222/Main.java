f76b00bbcd5ff228e7b182d232cd09af75d70b34
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-28 10:21:04 +0300

    s2_lab02_22 - basic functionality

--- s2_lab02_22_petrenko/src/com/petrenko/lab0222/Main.java
+++ s2_lab02_22_petrenko/src/com/petrenko/lab0222/Main.java
@@ -1,8 +1,33 @@
 package com.petrenko.lab0222;
 
+import java.io.Console;
+import java.util.Locale;
+
 public class Main {
 
-    public static void main(String[] args) {
-        IntegerDeposit id = new IntegerDeposit();
+    public static void main(String... args){
+        IntegerDeposit deposit = new IntegerDeposit();
+        Console console = System.console();
+        String input;
+
+        do {
+            input = console.readLine("Please make your input - {command} {value}: ").trim().toLowerCase(Locale.ROOT);
+            String[] params = input.split(" ");
+            int value = Integer.parseInt(params[1]);
+
+            switch (params[0]) {
+                case "add":
+                    deposit.addInteger(value);
+                    break;
+                case "check":
+                    deposit.checkDeposit();
+                    break;
+
+            }
+        } while (!input.equals("exit"));
+
+
+
+        console.printf("Bye.");
     }
 }
