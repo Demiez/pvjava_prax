d764a12db39efd4455803bcc72583dad0bb05fd1
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-28 11:50:36 +0300

    s2_lab02_22 - refactored to TreeSet

--- s2_lab02_22_petrenko/src/com/petrenko/lab0222/Main.java
+++ s2_lab02_22_petrenko/src/com/petrenko/lab0222/Main.java
@@ -2,9 +2,9 @@ package com.petrenko.lab0222;
 
 import java.io.Console;
 import java.util.Locale;
+import java.util.TreeSet;
 
 public class Main {
-
     public static void main(String... args){
         IntegerDeposit deposit = new IntegerDeposit();
         Console console = System.console();
@@ -13,21 +13,64 @@ public class Main {
         do {
             input = console.readLine("Please make your input - {command} {value}: ").trim().toLowerCase(Locale.ROOT);
             String[] params = input.split(" ");
-            int value = Integer.parseInt(params[1]);
+            int value = params.length == 2 ? Integer.parseInt(params[1]) : 0;
 
             switch (params[0]) {
                 case "add":
                     deposit.addInteger(value);
                     break;
+                case "del":
+                    deposit.removeInteger(value);
+                    break;
                 case "check":
                     deposit.checkDeposit();
                     break;
-
+                case "find":
+                    deposit.findInteger(value);
+                    break;
             }
         } while (!input.equals("exit"));
 
-
-
         console.printf("Bye.");
+    }
+}
+
+class IntegerDeposit {
+    public TreeSet<Integer> deposit = new TreeSet<Integer>();
+
+    public void addInteger(int number) {
+        deposit.add(number);
+        System.out.printf("Added integer: %d \n", number);
+    }
+
+    public void removeInteger(int number) {
+        if (deposit.contains(number)) {
+            deposit.remove(number);
+            System.out.printf("Removed integer: %d \n", number);
+        } else {
+            System.out.println("Sorry, there's no such number \n");
+        }
+    }
+
+    public void checkDeposit() {
+        System.out.println("Current deposit includes:");
+
+        for(Integer item: this.deposit){
+            System.out.println(item);
+        }
+    }
+
+    public void findInteger(int number) {
+        if (deposit.contains(number)) {
+            System.out.printf("Found your integer: %d \n", number);
+        } else {
+            var result = deposit.floor(number);
+
+            if (result == null) {
+                System.out.println("Sorry, couldn't find any number");
+            } else {
+                System.out.printf("Found closest integer by module: %d \n", result);
+            }
+        }
     }
 }
