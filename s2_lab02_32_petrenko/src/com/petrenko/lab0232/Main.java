9fb84fda8d136dc932dffe3498f68b8e21b95ea9
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-29 08:18:19 +0300

    s2_lab02_32 - init commit

---
+++ s2_lab02_32_petrenko/src/com/petrenko/lab0232/Main.java
@@ -0,0 +1,8 @@
+package com.petrenko.lab0232;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    PhoneBook pb = new PhoneBook();
+    }
+}
---
+++ s2_lab02_32_petrenko/src/com/petrenko/lab0232/PhoneBook.java
@@ -0,0 +1,7 @@
+package com.petrenko.lab0232;
+
+import java.util.HashMap;
+
+public class PhoneBook {
+    public HashMap<Integer,String> book = new HashMap<>();
+}
