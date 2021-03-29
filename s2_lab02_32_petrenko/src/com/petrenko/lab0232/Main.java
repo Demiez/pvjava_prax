d96a0f88de0d5ff5bb11bff6625d6fe16e26a7b0
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-03-29 09:12:33 +0300

    s2_lab02_32 - implemented HashMap

--- s2_lab02_32_petrenko/src/com/petrenko/lab0232/Main.java
+++ s2_lab02_32_petrenko/src/com/petrenko/lab0232/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	    PhoneBook pb = new PhoneBook();
+
+	    pb.addPhone(80675556677L, "Barchenko");
+        pb.addPhone(80674448877L, "Petrichenko");
+        pb.addPhone(80683334455L, "Pavlenko");
+        pb.addPhone(80502223344L, "Andrushko");
+        pb.addPhone(80501112299L, "Lyashenko");
+        pb.addPhone(80638881155L, "Bilyk");
+        pb.addPhone(80984707331L, "Petrichenko");
+
+        pb.checkAll();
+
+        pb.removePhone(80683334455L);
+        pb.checkAll();
+
+        pb.fingByNumber(80638881156L);
+        pb.fingByNumber(80638881155L);
+
+        pb.findBySurname("Petrichenk");
+        pb.findBySurname("Petrichenko");
     }
 }
--- s2_lab02_32_petrenko/src/com/petrenko/lab0232/PhoneBook.java
+++ s2_lab02_32_petrenko/src/com/petrenko/lab0232/PhoneBook.java
@@ -1,7 +1,51 @@
 package com.petrenko.lab0232;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class PhoneBook {
-    public HashMap<Integer,String> book = new HashMap<>();
+    public HashMap<Long,String> book = new HashMap<>();
+
+    public void addPhone (long phoneNumber, String surname) {
+        this.book.put(phoneNumber, surname);
+    }
+
+    public void removePhone (long phoneNumber) {
+        if (this.book.containsKey(phoneNumber)) {
+            this.book.remove(phoneNumber);
+            System.out.printf("Removed: %d \n", phoneNumber);
+        } else {
+            System.out.println("Can't find number");
+        }
+    }
+
+    public void checkAll () {
+        System.out.println("Current notes in phoneBook: ");
+        System.out.println(book);
+    }
+
+    public void fingByNumber(long phoneNumber) {
+        if (this.book.containsKey(phoneNumber)) {
+            System.out.printf("Such number exists, it belongs to: %s \n", this.book.get(phoneNumber));
+        } else {
+            System.out.println("Can't find number");
+        }
+    }
+
+    public void findBySurname(String surname) {
+        if (this.book.containsValue(surname)) {
+            System.out.println("Such surname exists, the notes are:");
+            Iterator it = this.book.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                if (pair.getValue() == surname) {
+                    System.out.println(pair.getKey() + " = " + pair.getValue());
+                }
+                it.remove();
+            }
+        } else {
+            System.out.println("Can't find surname");
+        }
+    }
 }
